1d20f0b29332cd7575f7130d7aa72d73358c7eef
juampiramallo <juampi2005.ca@gmail.com>
2024-06-24 11:35:06 -0300

    juampi de caja pandora

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  function convertirABinario(num) {
+    let binario = "";
+    while (num > 0) {
+      binario = (num % 2) + binario;
+      num = Math.floor(num / 2);
+    }
+    return binario || "0";
+  }
+  if (numero % 2 == 0) {
+    return convertirABinario(numero);
+  } else {
+    function convertirAHexadecimal(num) {
+      const hexDigitos = "0123456789ABCDEF";
+      let hexadecimal = "";
+      while (num > 0) {
+        hexadecimal = hexDigitos[num % 16] + hexadecimal;
+        num = Math.floor(num / 16);
+      }
+      return hexadecimal || "0";
+    }
+  }
 }
+
+console.log(laCajaDePandora(2));
+console.log(laCajaDePandora(9));
