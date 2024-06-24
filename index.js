19ed1fe5f91798eb6f3455729f394a699fa4f9d6
fatimdaruich <fatimdaruich.fd@gmail.com>
2024-06-24 11:32:46 -0300

    funcionpandora

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
+  // proximamente escribiremos codigo aqui
 }
