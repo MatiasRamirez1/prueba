fcbb6faeea7c917cae19bd577bb2eff0966ab0f5
Matías Ramirez <matiasramirezesc@gmail.com>
2024-06-24 11:32:19 -0300

    commit Mati

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  if (numero % 2 === 0) return numero.toString(2);
+  return numero.toString(16);
 }
