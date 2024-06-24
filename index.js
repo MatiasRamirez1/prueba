3e7257c78812b8fcc7381885cb83330b5aeabffe
Matías Ramirez <matiasramirezesc@gmail.com>
2024-06-24 11:37:30 -0300

    segundo commit mati

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function laCajaDePandora(numero) {
   if (numero % 2 === 0) return numero.toString(2);
   return numero.toString(16);
 }
+
+function mati() {
+  return {
+    nombre: "Matias",
+    edad: 23,
+    nacionalidad: "Argentina",
+  };
+}
